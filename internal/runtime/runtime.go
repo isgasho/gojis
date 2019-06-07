26cd7fa35bbcfac642be0fbeea8d458a5421c2e1
TimSatke <tsatke@deloitte.com>
2019-06-07 16:13:02 +0200

    Add comment on runtime.New

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -1,8 +1,8 @@
 package runtime
 
 import (
-	"github.com/rs/zerolog"
 	"github.com/TimSatke/gojis/internal/parser"
+	"github.com/rs/zerolog"
 )
 
 type Runtime struct {
@@ -11,6 +11,8 @@ type Runtime struct {
 	ast *parser.Ast
 }
 
+// New creates a new runtime using the given logger and
+// evaluating the given AST.
 func New(log zerolog.Logger, ast *parser.Ast) *Runtime {
 	r := new(Runtime)
 	r.log = log
