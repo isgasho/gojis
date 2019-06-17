e91e938f4d88b1c65acf89b4903a7c16b86b6d80
Tim Satke <tim.satke@gmail.com>
2019-06-17 17:26:44 +0200

    Added large benchmark

--- internal/runtime/agent/stack/stack_bench_test.go
+++ internal/runtime/agent/stack/stack_bench_test.go
@@ -1,11 +1,15 @@
 package stack
 
-import "testing"
+import (
+	"crypto/rand"
+	"testing"
+)
 
 var r interface{}
 
 func benchmarkStack(b *testing.B, s Stack) {
-	b.Run("Push", benchmarkPush(s))
+	b.Run("Push 3B", benchmarkPush(s))
+	b.Run("Push 8KB", benchmarkPushLarge(s))
 	b.Run("Push Pop", benchmarkPushPop(s))
 	b.Run("Peek", benchmarkPeek(s))
 }
@@ -17,6 +21,29 @@ func benchmarkPush(s Stack) func(b *testing.B) {
 
 		for i := 0; i < b.N; i++ {
 			s.Push("foo")
+		}
+	}
+}
+
+func benchmarkPushLarge(s Stack) func(b *testing.B) {
+	return func(b *testing.B) {
+		var data [8192]byte
+		rand.Read(data[:])
+
+		type t struct {
+			data [8192]byte
+		}
+		newT := func() t {
+			t := new(t)
+			t.data = data
+			return *t
+		}
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			s.Push(newT())
 		}
 	}
 }
