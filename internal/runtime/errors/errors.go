5e3c4e7f769700a23b82c6ce62d47e9f8b849296
Tim Satke <tim.satke@gmail.com>
2019-06-17 21:22:16 +0200

    Added godoc

--- internal/runtime/errors/errors.go
+++ internal/runtime/errors/errors.go
@@ -2,13 +2,19 @@ package errors
 
 import "fmt"
 
+// ErrorKind is the type of error an error can have.
 type ErrorKind uint8
 
+// Available error types used in the specification.
 const (
 	ErrorKindTypeError ErrorKind = iota
 	ErrorKindReferenceError
+	ErrorKindRangeError
 )
 
+// Error is an error that can be thrown during runtime.
+// When implementing the specification, maybe this layer will be
+// reduced to the ThrowXXXError function objects.
 type Error interface {
 	error
 	Kind() ErrorKind
@@ -25,6 +31,7 @@ func (e errorImpl) Kind() ErrorKind {
 	return e.kind
 }
 
+// NewTypeError creates a new type error with the given error.
 func NewTypeError(msg string) Error {
 	return errorImpl{
 		error: fmt.Errorf("TypeError: %v", msg),
@@ -32,9 +39,18 @@ func NewTypeError(msg string) Error {
 	}
 }
 
+// NewReferenceError creates a new reference error with the given error.
 func NewReferenceError(msg string) Error {
 	return errorImpl{
 		error: fmt.Errorf("ReferenceError: %v", msg),
 		kind:  ErrorKindReferenceError,
+	}
+}
+
+// NewRangeError creates a new range error with the given error.
+func NewRangeError(msg string) Error {
+	return errorImpl{
+		error: fmt.Errorf("RangeError: %v", msg),
+		kind:  ErrorKindRangeError,
 	}
 }
