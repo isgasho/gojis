89acfe59b5e77c58d7df85d5f6bd3f4d383ca3af
Tim Satke <tim.satke@gmail.com>
2019-06-19 23:35:51 +0200

    Improved godoc

--- internal/runtime/lang/object_operations.go
+++ internal/runtime/lang/object_operations.go
@@ -6,18 +6,26 @@ import (
 	"github.com/TimSatke/gojis/internal/runtime/errors"
 )
 
-// Get implements the function Get as specified in 7.3.1 in the specification.
-func Get(o *Object, p StringOrSymbol) (Value, errors.Error) {
+// Get retrieves the value of a specific property of an object.
+// Get is specified in 7.3.1.
+func Get(o *Object, p StringOrSymbol) (InternalValue, errors.Error) {
 	return o.Get(p, o)
 }
 
-// GetV implements the function GetV as specified in 7.3.2 in the specification.
+// GetV retrieves the value of a specific property of an object,
+// assuming that the value is an ECMAScript language value.
+// If the value is not an object, the property lookup is performed
+// using a wrapper object appropriate for the type of the value.
+// GetV is specified in 7.3.2.
 func GetV(v Value, p StringOrSymbol) (Value, errors.Error) {
 	o := ToObject(v)
 	return o.Get(p, v)
 }
 
-// Set implements the function Set as specified in 7.3.3 in the specification.
+// Set is used to set the value of a specific property of an object.
+// If throw == true and the property cannot be set, a TypeError will be returned
+// which then must be thrown.
+// Set is specified in 7.3.3.
 func Set(o *Object, p StringOrSymbol, v Value, throw bool) (Boolean, errors.Error) {
 	success, err := o.Set(p, v, o)
 	if err != nil {
@@ -31,19 +39,24 @@ func Set(o *Object, p StringOrSymbol, v Value, throw bool) (Boolean, errors.Erro
 	return success, nil
 }
 
-// CreateDataProperty implements the function CreateDataProperty as specified in 7.3.4 in the specification.
+// CreateDataProperty creates a new own property of an object.
+// CreateDataProperty is specified in 7.3.4.
 func CreateDataProperty(o *Object, p StringOrSymbol, v Value) Boolean {
 	desc := NewDataProperty(v, True, True, True)
 	return o.DefineOwnProperty(p, desc)
 }
 
-// CreateMethodProperty implements the function CreateMethodProperty as specified in 7.3.5 in the specification.
+// CreateMethodProperty creates a new own property of an object.
+// CreateMethodProperty is specified in 7.3.5.
 func CreateMethodProperty(o *Object, p StringOrSymbol, v Value) Boolean {
 	desc := NewDataProperty(v, True, False, True)
 	return o.DefineOwnProperty(p, desc)
 }
 
-// CreateDataPropertyOrThrow implements the function CreateDataPropertyOrThrow as specified in 7.3.6 in the specification.
+// CreateDataPropertyOrThrow creates a new own property of an object.
+// It returnes a TypeError exception if the requested property update cannot
+// be performed.
+// CreateDataPropertyOrThrow is specified in 7.3.6.
 func CreateDataPropertyOrThrow(o *Object, p StringOrSymbol, v Value) (Boolean, errors.Error) {
 	success := CreateDataProperty(o, p, v)
 	if !success {
@@ -52,7 +65,9 @@ func CreateDataPropertyOrThrow(o *Object, p StringOrSymbol, v Value) (Boolean, e
 	return success, nil
 }
 
-// DefinePropertyOrThrow implements the function DefinePropertyOrThrow as specified in 7.3.7 in the specification.
+// DefinePropertyOrThrow is used to call the DefineOwnProperty internal method of an object.
+// If the requested property update cannot be performed, a TypeError is returned.
+// DefinePropertyOrThrow is specified in 7.3.7.
 func DefinePropertyOrThrow(o *Object, p StringOrSymbol, desc *Property) (Boolean, errors.Error) {
 	success := o.DefineOwnProperty(p, desc)
 	if !success {
@@ -62,7 +77,9 @@ func DefinePropertyOrThrow(o *Object, p StringOrSymbol, desc *Property) (Boolean
 	return success, nil
 }
 
-// DeletePropertyOrThrow implements the function DeletePropertyOrThrow as specified in 7.3.8 in the specification.
+// DeletePropertyOrThrow removes a specific own property of an object.
+// If the property is not configurable, an exception is returned.
+// DeletePropertyOrThrow is specified in 7.3.8.
 func DeletePropertyOrThrow(o *Object, p StringOrSymbol) (Boolean, errors.Error) {
 	success := o.Delete(p)
 	if !success {
@@ -72,7 +89,9 @@ func DeletePropertyOrThrow(o *Object, p StringOrSymbol) (Boolean, errors.Error) 
 	return success, nil
 }
 
-// GetMethod implements the function GetMethod as specified in 7.3.9 in the specification.
+// GetMethod retrieves a callable object of a property of an ECMAScript language value.
+// If no error is returned, the object is guaranteed to be callable.
+// GetMethod is specified in 7.3.9.
 func GetMethod(v Value, p StringOrSymbol) (Value, errors.Error) {
 	f, err := GetV(v, p)
 	if err != nil {
@@ -90,17 +109,22 @@ func GetMethod(v Value, p StringOrSymbol) (Value, errors.Error) {
 	return f, nil
 }
 
-// HasProperty implements the function HasProperty as specified in 7.3.10 in the specification.
+// HasProperty is used to determine whether an object has a property with the specified property
+// key. The property can either be inherited or an own property of the object.
+// HasProperty is specified in 7.3.10.
 func HasProperty(o *Object, p StringOrSymbol) Boolean {
 	return o.HasProperty(p)
 }
 
-// HasOwnProperty implements the function HasOwnProperty as specified in 7.3.11 in the specification.
+// HasOwnProperty is used to determine whether an object has a property with the specified property
+// key. The property can only be an own property of the object.
+// HasProperty is specified in 7.3.11.
 func HasOwnProperty(o *Object, p StringOrSymbol) Boolean {
 	return o.GetOwnProperty(p) != nil
 }
 
-// Call implements the function Call as specified in 7.3.12 in the specification.
+// Call is used to call the Call internal method of a function object.
+// Call is specified in 7.3.12.
 func Call(f *Object, thisValue Value, args ...Value) (Value, errors.Error) {
 	if args == nil {
 		args = []Value{}
@@ -113,7 +137,8 @@ func Call(f *Object, thisValue Value, args ...Value) (Value, errors.Error) {
 	return f.Call(thisValue, args...)
 }
 
-// Construct implements the function Construct as specified in 7.3.13 in the specification.
+// Construct is used to call the Construct internal method of a constructor object.
+// Construct is specified in 7.3.13.
 func Construct(f, newTarget *Object, args ...Value) (*Object, errors.Error) {
 	if newTarget == nil {
 		newTarget = f
@@ -132,7 +157,8 @@ const (
 	IntegrityLevelFrozen = "frozen"
 )
 
-// SetIntegrityLevel implements the function SetIntegrityLevel as specified in 7.3.14 in the specification.
+// SetIntegrityLevel is used to fix the set of own properties of an object.
+// SetIntegrityLevel is specified in 7.3.14.
 func SetIntegrityLevel(o *Object, level string) (Boolean, errors.Error) {
 	status := o.PreventExtensions()
 	if !status {
@@ -182,7 +208,9 @@ func setIntegrityLevelFrozen(o *Object, keys []StringOrSymbol) (Boolean, errors.
 	return True, nil
 }
 
-// TestIntegrityLevel implements the function TestIntegrityLevel as specified in 7.3.15 in the specification.
+// TestIntegrityLevel is used to determine if the set of own properties of an object
+// are fixed.
+// TestIntegrityLevel is specified in 7.3.15.
 func TestIntegrityLevel(o *Object, level string) Boolean {
 	if o.IsExtensible() {
 		return False
@@ -205,17 +233,21 @@ func TestIntegrityLevel(o *Object, level string) Boolean {
 	return true
 }
 
-// CreateArrayFromList implements the function CreateArrayFromList as specified in 7.3.16 in the specification.
-func CreateArrayFromList() {
+// CreateArrayFromList creates an array whose elements are provided by a List.
+// CreateArrayFromList is specified in 7.3.16.
+func CreateArrayFromList(elements []Value) {
 	panic("TODO: Arrays")
 }
 
-// CreateListFromArrayLike implements the function CreateListFromArrayLike as specified in 7.3.17 in the specification.
-func CreateListFromArrayLike() {
+// CreateListFromArrayLike creates a List value whose elements are provided by the indexed
+// properties of an array-like object.
+// CreateListFromArrayLike is specified in 7.3.17.
+func CreateListFromArrayLike(o *Object, elementTypes []Type) {
 	panic("TODO: Arrays")
 }
 
-// Invoke implements the function Invoke as specified in 7.3.18 in the specification.
+// Invoke is used to call a method property of an ECMAScript language value.
+// Invoke is specified in 7.3.18.
 func Invoke(v Value, p StringOrSymbol, args ...Value) (Value, errors.Error) {
 	if args == nil {
 		args = []Value{}
@@ -229,22 +261,31 @@ func Invoke(v Value, p StringOrSymbol, args ...Value) (Value, errors.Error) {
 	return Call(f.(*Object), v, args...)
 }
 
-// OrdinaryHasInstance implements the function OrdinaryHasInstance as specified in 7.3.19 in the specification.
-func OrdinaryHasInstance() {
+// OrdinaryHasInstance implements the default algorithm for determining if an object o inherits
+// from the instance object inheritance path provided by constructor c.
+// OrdinaryHasInstance is specified in 7.3.19.
+func OrdinaryHasInstance(c, o *Object) Boolean {
 	panic("TODO")
 }
 
-// SpeciesConstructor implements the function SpeciesConstructor as specified in 7.3.20 in the specification.
+// SpeciesConstructor retrieves the constructor that should be used to create new objects
+// that are derived from the argument object o. The defaultConstructor argument is the
+// constructor to use if a constructor's @@species property cannot be found starting from o.
+// SpeciesConstructor is specified in 7.3.20.
 func SpeciesConstructor() {
 	panic("TODO")
 }
 
-// EnumerableOwnPropertyNames implements the function EnumerableOwnPropertyNames as specified in 7.3.21 in the specification.
+// EnumberableOwnPropertyNames is specified in 7.3.21.
 func EnumerableOwnPropertyNames() {
 	panic("TODO")
 }
 
-// CopyDataProperties implements the function CopyDataProperties as specified in 7.3.23 in the specification.
+// 7.3.22 GetFunctionRealm is not implemented here. You will find it in
+// internal/runtime/realm/relam.go. The reason for that is, that otherwise,
+// an import cycle lang -> realm -> lang rises.
+
+// CopyDataProperties is specified in 7.3.23.
 func CopyDataProperties() {
 	panic("TODO")
 }
