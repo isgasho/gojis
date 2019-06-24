736c11a2782c2f7270b0f8f36d0b8ff127e0f25d
Tim Satke <tim.satke@gmail.com>
2019-06-24 22:46:46 +0200

    Add godoc comments

--- internal/runtime/binding/binding.go
+++ internal/runtime/binding/binding.go
@@ -2,6 +2,8 @@ package binding
 
 import "github.com/TimSatke/gojis/internal/runtime/lang"
 
+// Binding represents a concept described throughout the specification,
+// especially in context with environment records.
 type Binding struct {
 	name      lang.String
 	value     lang.Value
@@ -11,40 +13,49 @@ type Binding struct {
 	strict    bool
 }
 
+// NewBinding creates a new named binding with the given name.
 func NewBinding(name lang.String) *Binding {
 	b := new(Binding)
 	b.name = name
 	return b
 }
 
+// Name returns the name of this binding.
 func (b *Binding) Name() lang.String {
 	return b.name
 }
 
+// Set assigns a value to this binding.
 func (b *Binding) Set(val lang.Value) {
 	b.value = val
 }
 
+// Value returns the value of this binding.
 func (b *Binding) Value() lang.Value {
 	return b.value
 }
 
+// IsStrict is used to determine whether this binding is a strict binding.
 func (b *Binding) IsStrict() bool {
 	return b.strict
 }
 
+// IsFinal is used to determine whether this binding is final.
 func (b *Binding) IsFinal() bool {
 	return b.final
 }
 
+// IsInitialized is used to determine whether this binding is initialized.
 func (b *Binding) IsInitialized() bool {
 	return b.value != nil
 }
 
+// IsDeletable is used to determine whether this binding is deletable.
 func (b *Binding) IsDeletable() bool {
 	return b.deletable
 }
 
+// IsImmutable is used to determine whether this binding is immutable.
 func (b *Binding) IsImmutable() bool {
 	return b.immutable
 }
--- internal/runtime/binding/environment_constructors.go
+++ internal/runtime/binding/environment_constructors.go
@@ -2,6 +2,12 @@ package binding
 
 import "github.com/TimSatke/gojis/internal/runtime/lang"
 
+// GetIdentifierReference returns a named reference with n as the name.
+// If strict is true, the reference will be a strict reference.
+// The given environment must be of type Environment or Null.
+// If the environment does not have a binding with the given name, its outer
+// environment is used.
+// GetIdentifierReference is specified in 8.1.2.1.
 func GetIdentifierReference(env lang.InternalValue, n lang.String, strict bool) *Reference {
 	if env == lang.Null {
 		return NewReference(lang.NewStringOrSymbol(n), lang.Undefined, strict)
@@ -15,6 +21,9 @@ func GetIdentifierReference(env lang.InternalValue, n lang.String, strict bool) 
 	return GetIdentifierReference(lex.Outer(), n, strict)
 }
 
+// NewDeclarativeEnvironment creates a new declarative environment with
+// the given outer environment.
+// NewDeclarativeEnvironment is specified in 8.1.2.2.
 func NewDeclarativeEnvironment(outer Environment) *DeclarativeEnvironment {
 	env := new(DeclarativeEnvironment)
 	env.outer = outer
@@ -22,6 +31,9 @@ func NewDeclarativeEnvironment(outer Environment) *DeclarativeEnvironment {
 	return env
 }
 
+// NewObjectEnvironment creates a new object environment with
+// the given outer environment for the given object.
+// NewObjectEnvironment is specified in 8.1.2.3.
 func NewObjectEnvironment(outer Environment, obj *lang.Object) *ObjectEnvironment {
 	env := new(ObjectEnvironment)
 	env.outer = outer
@@ -29,11 +41,20 @@ func NewObjectEnvironment(outer Environment, obj *lang.Object) *ObjectEnvironmen
 	return env
 }
 
-func NewFunctionEnvironment(outer Environment, f interface{}) *FunctionEnvironment {
-	panic("TODO: ECMAScript function objects")
+// NewFunctionEnvironment creates a new function environment with
+// the given outer environment for the given function object.
+// The passed function object can either be Undefined or an Object.
+// NewFunctionEnvironment is specified in 8.1.2.4.
+func NewFunctionEnvironment(outer Environment, f lang.Value) *FunctionEnvironment {
+	lang.EnsureTypeOneOf(f, lang.TypeObject, lang.TypeUndefined)
+
+	panic("TODO: 8.1.2.4 NewFunctionEnvironment")
 }
 
-func NewGlobalEnvironment(globalObj *lang.Object, thisValue *lang.Object) *GlobalEnvironment {
+// NewGlobalEnvironment creates a new global environment with the given global object and
+// the given thisValue as this value.
+// NewGlobalEnvironment is specified in 8.1.2.5.
+func NewGlobalEnvironment(globalObj, thisValue *lang.Object) *GlobalEnvironment {
 	env := new(GlobalEnvironment)
 	objRec := NewObjectEnvironment(env, globalObj)
 	dclRec := NewDeclarativeEnvironment(env)
@@ -44,6 +65,8 @@ func NewGlobalEnvironment(globalObj *lang.Object, thisValue *lang.Object) *Globa
 	return env
 }
 
+// NewModuleEnvironment creates a new module environment with the given environment as
+// outer environment.
 func NewModuleEnvironment(outer Environment) *ModuleEnvironment {
 	env := new(ModuleEnvironment)
 	env.outer = outer
